9ad35571de46756c286954be919d1c869f55470c
mario <mario.mazzarelli@asfweb.net>
2021-09-11 18:57:47 +0200

    .....

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 var multer = require("multer");
-var upload = multer({ dest: "file/" });
+const { readFile } = require("fs/promises");
+var upload = multer({ dest: "src/file/" });
 const ip = require("ip").address();
-//console.dir(ip.address());
-
 const app = express();
-//app.use(bodyParser.urlencoded({ extended: true }));
 
-// parse application/x-www-form-urlencoded
+// initial configurations
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
 app.use(bodyParser.json());
 
 const port = 3000;
@@ -20,33 +17,44 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("il server funziona");
 });
+// ^ initial configurations
 
-//app flutter
+// functions
+function readDirectory() {
+  fs.readdir(__dirname + "/file", function (err, files) {
+    if (err) {
+      console.log("Unable to scan directory: " + err);
+    }
+    return files;
+  });
+}
+// ^ functions
 
-//registered users
+// get all file
 
-app.post("/registered-users", (req, res) => {
-  dataRegistration = req.body;
-
-  console.log(dataRegistration);
-
-  res.send({ state: "success" });
+app.get("/allFiles", (req, res) => {
+  var files_ = new Array();
+  fs.readdir(__dirname + "/file", function (err, files) {
+    if (err) {
+      return console.log("Unable to scan directory: " + err);
+    }
+    files.forEach(function (file) {
+      console.log(file);
+      files_.push(file);
+    });
+  });
+  console.log(files_);
+  res.send(files_);
 });
 
-//end registered users
+// ^ get all file
 
-//end app flutter
-
-//app flutter receive file
+// app flutter receive file
 
 app.post("/receive-file", upload.single("file"), function (req, res) {
-  var lines = process.stdout.getWindowSize()[1];
-  for (var i = 0; i < lines; i++) {
-    console.log("\r\n");
-  }
   console.log("Received file" + req.file.originalname);
   var src = fs.createReadStream(req.file.path);
-  var dest = fs.createWriteStream("file/" + req.file.originalname);
+  var dest = fs.createWriteStream("src/file/" + req.file.originalname);
   src.pipe(dest);
   src.on("end", function () {
     fs.unlinkSync(req.file.path);
@@ -58,24 +66,8 @@ app.post("/receive-file", upload.single("file"), function (req, res) {
   });
 });
 
-//end app flutter  receive file
-
-//reports
-
-let reports = requestJson("json/reports/reports.json");
-
-function requestJson(address) {
-  let dataFile = fs.readFileSync(__dirname + "/" + address, "utf8");
-  let dataParse = JSON.parse(dataFile);
-  return dataParse;
-}
-
-app.get("/reports", (req, res) => {
-  res.send(reports);
-});
-
-//end reports
+// end app flutter  receive file
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://${ip}:${port}`);
+  console.log(`server listening at http://${ip}:${port}`);
 });
